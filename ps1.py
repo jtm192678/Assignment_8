c39a431ab9a366e53b59fcb3464448411dc80394
suraj <suraj.parihar004@gmail.com>
2019-09-18 15:51:18 +0530

    final

--- ps1.py
+++ ps1.py
@@ -10,17 +10,21 @@ def check(string):                   #function to check for a valid string
 		print("please enter again a valid binary string")	
 
 if __name__ == "__main__" : 
-	string=input("please enter a valid binary string\n")
+	string=input("please enter a valid binary string:   ")
 	check(string)                     #calling check function
+
+
 substring='1'
 count=string.count(substring)
 #print("count is:",count)
 if count%2==0:                                         #to add parity bit at the end of the binary data
-	string=string+'1'
-	print("The parity corrected data is:  ",string)      #print parity added data
+	string2=string+'1'
+	print("The parity corrected data is:",string2)      #print parity added data
 else:
-	string=string+'0'
-	print("The parity corrected data is:  ",string)
-print("\n")
-string2=string.replace("010","0100")                      #replacing the '010' substring by '0100'
-print("The transmitting data is:   ",string2)
+	string2=string+'0'
+	print("The parity corrected data is:\n  ",string2)
+
+
+string3=string.replace("010","0100")                      #replacing the '010' substring by '0100'
+string4=string3+"0101"
+print("The transmitting data is:",string4)
